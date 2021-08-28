80814273c50a022f77f9c8664ea41333d153b315
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-08-28 15:47:42 -0500

    finalizando sistema condicional

--- scripts/answers.js
+++ scripts/answers.js
@@ -1,23 +1,47 @@
 let template = document.getElementById('template').content;
 let items = document.getElementById('items');
 let menu = document.getElementById('menu');
+let comprobar = document.querySelector('.comprobar');
 let fragment = document.createDocumentFragment();
 
 let seleccion = false;
 let control = [];
+let porcentaje = 0;
+let vida = 0;
 
-if (localStorage.getItem('comprobar') === null) {
-    control = [];
-} else {
-    control = localStorage.getItem('comprobar')
-    control = JSON.parse(control);
-}
+window.addEventListener('beforeunload', () =>{
+    alert('reload')
+});
 
-document.addEventListener('DOMContentLoaded', () => {
-    idAleatorio();
+document.addEventListener('DOMContentLoaded', inicio = () => {
+    items.innerHTML = '';
+    valoresIni();
 })
 
+
+ 
+const valoresIni = () =>{
+    if (localStorage.getItem('vidas') === null) {
+        vida = 4;
+        document.getElementById('vida').textContent = vida;
+    } else {
+        vida = localStorage.getItem('vidas');
+        document.getElementById('vida').textContent = vida;
+    }
+    
+         
+    if (localStorage.getItem('comprobar') === null) {
+        control = [];
+    } else {
+        control = localStorage.getItem('comprobar')
+        control = JSON.parse(control);
+    }
+
+    idAleatorio();
+}
+
 const idAleatorio = () => {
+   
     if (control.length == 0) {//si el objeto control no tiene nada
         //lanza un aleatorio del 1 al 3
         let id = parseInt((Math.random() * (3 - 1 + 1)) + 1);
@@ -31,27 +55,31 @@ const idAleatorio = () => {
         let id = parseInt((Math.random() * (3 - 1 + 1)) + 1);
         const omitir = control; //numero que debemos omitir para que no se repita almacenado en el array
         //repita mientras que el numero que me dio sea igual al numero que quiero omitir
-        while(omitir == id){
-        id = parseInt((Math.random() * (3 - 1 + 1)) + 1);
+        while (omitir == id) {
+            id = parseInt((Math.random() * (3 - 1 + 1)) + 1);
         }
         control.push(id);
         console.log(control)
+        porcentaje = 25;
         localStorage.setItem('comprobar', JSON.stringify(control));
         getData(id);
-    }else if(control.length ==2){
+        progresion(porcentaje);
+    } else if (control.length == 2) {
         let id = parseInt((Math.random() * (3 - 1 + 1)) + 1);
-        const omitir = control; 
-        while(omitir[0] == id || omitir[1] == id){
-        id = parseInt((Math.random() * (3 - 1 + 1)) + 1);
+        const omitir = control;
+        while (omitir[0] == id || omitir[1] == id) {
+            id = parseInt((Math.random() * (3 - 1 + 1)) + 1);
         }
         control.push(id);
         console.log(control)
+        porcentaje = 50;
         localStorage.setItem('comprobar', JSON.stringify(control));
         getData(id);
+        progresion(porcentaje);
     }
 }
 
-const getData = async (id) => {
+const getData = async (id) => {//enviamos el id resultante de cada caso
     let res = await fetch(`http://localhost:4000/pregunta${id}`)
     let preg = await res.json();
     pintarData(preg);
@@ -61,7 +89,7 @@ const pintarData = data => {
     items.innerHTML = '';
     console.log(data);
     data.forEach(preg => {
-        const { image, pregunta, op1, op2, op3, id } = preg;
+        const { image, pregunta, op1, op2, op3 } = preg;
         template.getElementById('image').setAttribute('src', image);
         template.getElementById('answer').textContent = pregunta;
         template.getElementById('option1').textContent = op1;
@@ -79,63 +107,75 @@ const condicionales = () => {
     let option2 = document.getElementById('option2');
     let option3 = document.getElementById('option3');
 
+    
 
     option1.addEventListener('click', (e) => {
         e.stopPropagation();
+        pintarBoton();
         if (option1.textContent == "main") {
-            respuestaCorrecta();
             seleccion = true;
-        } else {
-            respuestaIncorrecta();
-        }
+            resolucion(seleccion);
+        }       
+        
     })
 
     option2.addEventListener('click', (e) => {
         e.stopPropagation();
+        pintarBoton();
         if (option2.textContent == "<script>" || option2.textContent == "Don’t repeat yourself") {
-            respuestaCorrecta();
             seleccion = true;
-        } else {
-            respuestaIncorrecta();
+            resolucion(seleccion);
         }
+        
     })
 
     option3.addEventListener('click', (e) => {
         e.stopPropagation();
-        respuestaIncorrecta();
+        pintarBoton();
+        
     })
-
+   
+    resolucion();
+    
 }
 
-const respuestaIncorrecta = () => {
-    menu.querySelector('button').setAttribute('id', 'comprobarCambio');
-    let comprobar = document.getElementById('comprobarCambio');
+const resolucion = (select) => {
+    console.log(seleccion)
 
-    comprobar.addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log('incorrecta');
-    })
+    comprobar.addEventListener('click', () => {
 
-}
-
-const respuestaCorrecta = () => {
-
-    menu.querySelector('button').setAttribute('id', 'comprobarCambio');
-    let comprobar = document.getElementById('comprobarCambio');
-
-    comprobar.addEventListener('click', (e) => {
-        e.stopPropagation();
-        if (seleccion == true) {
-            // console.log(control);
+        if(seleccion==true){
             console.log('correcto')
             location.reload();
-        } else {
+        }else{
             console.log('incorrecto')
-
+            vida--
+            vidas(vida)
         }
+
 
     })
 
 }
 
+
+const pintarBoton = () => {
+    menu.querySelector('button').setAttribute('id', 'comprobarCambio');
+}
+
+const progresion = (porcentaje) => {
+    document.getElementById('progresion').setAttribute('style', `width: ${porcentaje}%`)
+}
+
+const vidas = (vida) => {
+    console.log(vida)
+    if (vida <= 0) {
+        console.log('Game Over')
+        localStorage.clear();
+        window.location.href = './home.html'
+    } else {
+        document.getElementById('vida').textContent = vida;
+        localStorage.setItem('vidas', vida);
+    }
+}
 
