b79935fcd1ee5c271023e037799834b06f11ca36
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-08-29 16:00:34 -0500

    finalizando pregunta 5

--- aswersTemplate.md
+++ aswersTemplate.md
@@ -23,3 +23,42 @@
             </label>
         </div>
 </template>
+
+    <div id="preguntaImgSel">
+            <span id="answer"></span>
+        </div>
+
+        <div id="respuestaImgSel">
+            <div>
+                <input type="radio" name="input" id="resp1ImgSel" value="">
+                <label class="b" for="resp1ImgSel" id="optio1">
+                    <img src="./resources/answers/angular.png" alt="" srcset="">
+                    <span id="resp1"></span>
+                </label>
+
+                <input type="radio" name="input" id="resp2ImgSel" value="">
+                <label class="b" for="resp2ImgSel" id="option3">
+                    <img src="./resources/answers/windows.png" alt="" srcset="">
+                    <span id="resp2"></span>
+                </label>
+            </div>
+            <div>
+                <input type="radio" name="input" id="resp3ImgSel" value="">
+                <label class="b" for="resp3ImgSel" id="option2">
+                    <img src="./resources/answers/vue.png" alt="" srcset="">
+                    <span id="resp3"></span>
+                </label>
+
+                <input type="radio" name="input" id="resp4ImgSel" value="">
+                <label class="b" for="resp4ImgSel" id="option4">
+                    <img src="./resources/answers/kotlin.png" alt="" srcset="">
+                    <span id="resp4"></span>
+                </label>
+            </div>
+
+            <!-- <h5><strong>¡Buen Trabajo!</strong></h5>
+            <button id="continue">CONTINUAR</button>
+
+            <!-- <h5><strong>La Respuesta Correcta es:</strong></h5>
+            <span>main</span>
+            <button id="inContinue">CONTINUAR</button> -->
--- scripts/answers.js
+++ scripts/answers.js
@@ -1,6 +1,10 @@
-let template = document.getElementById('template').content;
+let templateSel = document.getElementById('templateSel').content;
+let templateSelImg = document.getElementById('templateSelImg').content;
+let templateGood = document.getElementById('good').content;
+let templateBad = document.getElementById('bad').content;
+
 let items = document.getElementById('items');
-let menu = document.getElementById('menu');
+let footer = document.getElementById('footer');
 let comprobar = document.querySelector('.comprobar');
 let fragment = document.createDocumentFragment();
 
@@ -39,81 +43,154 @@ const valoresIni = () => {
 
 const idAleatorio = () => {
 
-    if (control.length == 0) {//si el objeto control no tiene nada
-        //lanza un aleatorio del 1 al 3
-        let id = parseInt((Math.random() * (3 - 1 + 1)) + 1);
-        //cargalo en el local storage para llevar el control
-        control.push(id);
-        localStorage.setItem('comprobar', JSON.stringify(control));
-        getData(id);
-    } else if (control.length == 1) {//si el objeto control tiene cargado ya un numero
-        //lanza un aleatorio cualquiera
-        let id = parseInt((Math.random() * (3 - 1 + 1)) + 1);
-        const omitir = control; //numero que debemos omitir para que no se repita almacenado en el array
-        //repita mientras que el numero que me dio sea igual al numero que quiero omitir
-        while (omitir == id) {
-            id = parseInt((Math.random() * (3 - 1 + 1)) + 1);
-        }
-        control.push(id);
-        console.log(control)
-        porcentaje = 25;
-        localStorage.setItem('comprobar', JSON.stringify(control));
-        getData(id);
-        progresion(porcentaje);
-    } else if (control.length == 2) {
-        let id = parseInt((Math.random() * (3 - 1 + 1)) + 1);
-        const omitir = control;
-        while (omitir[0] == id || omitir[1] == id) {
-            id = parseInt((Math.random() * (3 - 1 + 1)) + 1);
-        }
-        control.push(id);
-        console.log(control)
-        porcentaje = 50;
-        localStorage.setItem('comprobar', JSON.stringify(control));
-        getData(id);
-        progresion(porcentaje);
-    } else if (control.length == 3) {
-        location.reload();
-        localStorage.clear();
-    }
-}
+    if (control.length == 0) {
 
+        let id = parseInt((Math.random() * (5 - 1 + 1)) + 1);
+        control.push(id);
+        localStorage.setItem('comprobar', JSON.stringify(control));
+        getData(id);
+
+    } else if (control.length == 1) {
+
+        let n = 0;
+        let id = localStorage.getItem('comprobar');
+        id = JSON.parse(id);
+
+        do { n = parseInt((Math.random() * (5 - 1 + 1)) + 1); }
+
+        while (control[0] == n);
+
+        control.push(n);
+        porcentaje = 20;
+        localStorage.setItem('comprobar', JSON.stringify(control));
+        getData(n);
+        progresion(porcentaje);
+
+    } else if (control.length == 2) {
+
+        let n = 0;
+        let id = localStorage.getItem('comprobar');
+        id = JSON.parse(id);
+
+        do { n = parseInt((Math.random() * (5 - 1 + 1)) + 1); }
+
+        while (control[0] == n || control[1] == n);
+
+        control.push(n);
+        console.log(control)
+        porcentaje = 40;
+        localStorage.setItem('comprobar', JSON.stringify(control));
+        getData(n);
+        progresion(porcentaje);
+
+    } else if (control.length == 3) {
+
+        let n = 0;
+        let id = localStorage.getItem('comprobar');
+        id = JSON.parse(id);
+
+        do { n = parseInt((Math.random() * (5 - 1 + 1)) + 1); }
+
+        while (control[0] == n || control[1] == n || control[2] == n);
+
+        control.push(n);
+        porcentaje = 60;
+        localStorage.setItem('comprobar', JSON.stringify(control));
+        getData(n);
+        progresion(porcentaje);
+
+    } else if (control.length == 4) {
+
+        let n = 0;
+        let id = localStorage.getItem('comprobar');
+        id = JSON.parse(id);
+
+        do { n = parseInt((Math.random() * (5 - 1 + 1)) + 1); }
+
+        while (control[0] == n || control[1] == n || control[2] == n || control[3] == n);
+
+        control.push(n);
+        porcentaje = 80;
+        localStorage.setItem('comprobar', JSON.stringify(control));
+        getData(n);
+        progresion(porcentaje);
+    }
+
+
+
+}
 const getData = async (id) => {//enviamos el id resultante de cada caso
     let res = await fetch(`http://localhost:4000/pregunta${id}`)
     let preg = await res.json();
-    pintarData(preg);
+
+    if (id > 0 && id <= 3) {
+        pintarDataSel(preg);
+    }else if (id == 4){
+        pintarDataDrag(preg);
+    }else if (id == 5){
+        pintarDataSelImg(preg);
+    }
 }
 
-const pintarData = data => {
+const pintarDataSel = data => {
     items.innerHTML = '';
 
     data.forEach(preg => {
-        const { image, pregunta, op1, op2, op3 } = preg;
-        template.getElementById('image').setAttribute('src', image);
-        template.getElementById('answer').textContent = pregunta;
-        template.getElementById('option1').textContent = op1;
-        template.getElementById('option2').textContent = op2;
-        template.getElementById('option3').textContent = op3;
-        fragment.appendChild(template);
+        const { image, pregunta, op1, op2, op3, respuesta } = preg;
+        templateSel.getElementById('image').setAttribute('src', image);
+        templateSel.getElementById('answer').textContent = pregunta;
+        templateSel.getElementById('option1').textContent = op1;
+        templateSel.getElementById('option2').textContent = op2;
+        templateSel.getElementById('option3').textContent = op3;
+        localStorage.setItem('good', respuesta);
+        fragment.appendChild(templateSel);
     })
     items.appendChild(fragment);
-    condicionales();
+    condicionalSel();
 }
 
-const condicionales = () => {
+const pintarDataDrag = data =>{
+    items.innerHTML = '';
+    console.log('es drag')
+}
+
+const pintarDataSelImg = data =>{
+    items.innerHTML = '';
+    
+    data.forEach(preg =>{
+        const {pregunta, imageOp1, imageOp2, imageOp3, imageOp4, op1, op2, op3, op4, respuesta} = preg;
+        templateSelImg.getElementById('answer').textContent = pregunta;
+        templateSelImg.getElementById('imgop1').setAttribute('src', imageOp1);
+        templateSelImg.getElementById('imgop2').setAttribute('src', imageOp2);
+        templateSelImg.getElementById('imgop3').setAttribute('src', imageOp3);
+        templateSelImg.getElementById('imgop4').setAttribute('src', imageOp4);
+        templateSelImg.getElementById('resp1').textContent = op1;
+        templateSelImg.getElementById('resp2').textContent = op2;
+        templateSelImg.getElementById('resp3').textContent = op3;
+        templateSelImg.getElementById('resp4').textContent = op4;
+        localStorage.setItem('good', respuesta);
+        fragment.appendChild(templateSelImg);
+    })
+    items.appendChild(fragment);
+    condicionalSelImg();
+}
+
+const condicionalSel = () => {
 
     let option1 = document.getElementById('option1');
     let option2 = document.getElementById('option2');
     let option3 = document.getElementById('option3');
+    let correct = localStorage.getItem('good');
 
     option1.addEventListener('click', (e) => {
 
         pintarBoton();
 
-        if (option1.textContent == "main") {
+        if (option1.textContent == correct) {
             localStorage.setItem('respuesta', 'correcto');
-        }else{
+        } else {
             localStorage.setItem('respuesta', 'incorrecto');
+            option1.setAttribute('class', 'a temp');
         }
         e.stopPropagation();
     })
@@ -122,10 +199,11 @@ const condicionales = () => {
 
         pintarBoton();
 
-        if (option2.textContent == "<script>" || option2.textContent == "Don’t repeat yourself") {
+        if (option2.textContent == correct) {
             localStorage.setItem('respuesta', 'correcto');
-        }else{
+        } else {
             localStorage.setItem('respuesta', 'incorrecto');
+            option2.setAttribute('class', 'a temp');
         }
         e.stopPropagation();
     })
@@ -133,37 +211,177 @@ const condicionales = () => {
     option3.addEventListener('click', (e) => {
 
         pintarBoton();
-        localStorage.setItem('respuesta', 'incorrecto');
+
+        if (option3.textContent == correct) {
+            localStorage.setItem('respuesta', 'correcto');
+        } else {
+            localStorage.setItem('respuesta', 'incorrecto');
+            option3.setAttribute('class', 'a temp');
+        }
         e.stopPropagation();
     })
 
-    respuesta()
+    respuestaSel()
 }
 
-const respuesta = () => {
+const condicionalSelImg = () =>{
+    let option1 = document.getElementById('option1');
+    let option2 = document.getElementById('option2');
+    let option3 = document.getElementById('option3');
+    let option4 = document.getElementById('option4');
+
+    let res1 = document.getElementById('resp1');
+    let res2 = document.getElementById('resp2');
+    let res3 = document.getElementById('resp3');
+    let res4 = document.getElementById('resp4');
+
+    let correct = localStorage.getItem('good');
+
+    option1.addEventListener('click', (e) => {
+
+        pintarBoton();
+
+        if (res1.textContent == correct) {
+            localStorage.setItem('respuesta', 'correcto');
+        } else {
+            localStorage.setItem('respuesta', 'incorrecto');
+            option1.setAttribute('class', 'b temp');
+        }
+        e.stopPropagation();
+    })
+
+    option2.addEventListener('click', (e) => {
+
+        pintarBoton();
+
+        if (res2.textContent == correct) {
+            localStorage.setItem('respuesta', 'correcto');
+        } else {
+            localStorage.setItem('respuesta', 'incorrecto');
+            option2.setAttribute('class', 'b temp');
+        }
+        e.stopPropagation();
+    })
+
+    option3.addEventListener('click', (e) => {
+
+        pintarBoton();
+
+        if (res3.textContent == correct) {
+            localStorage.setItem('respuesta', 'correcto');
+        } else {
+            localStorage.setItem('respuesta', 'incorrecto');
+            option3.setAttribute('class', 'b temp');
+        }
+        e.stopPropagation();
+    })
+
+    option4.addEventListener('click', (e) => {
+
+        pintarBoton();
+
+        if (res4.textContent == correct) {
+            localStorage.setItem('respuesta', 'correcto');
+        } else {
+            localStorage.setItem('respuesta', 'incorrecto');
+            option4.setAttribute('class', 'b temp');
+        }
+        e.stopPropagation();
+    })
+
+    respuestaSelImg();
+}
+
+
+//--------------
+
+const respuestaSel = () => {
 
     comprobar.addEventListener('click', (e) => {
         let res = localStorage.getItem('respuesta')
-        
-        if(res == 'incorrecto'){
-            console.log('incorrecto')
+
+        if (res == 'incorrecto') {
             localStorage.removeItem('respuesta');
             vida--
             vidas(vida);
+            incorrectoSel();
         }
         if (res === 'correcto') {
-            console.log('correcto')
             localStorage.removeItem('respuesta');
-            location.reload();
+            correcto();
         }
         e.stopPropagation();
     })
 
 }
 
+const respuestaSelImg = () => {
+
+    comprobar.addEventListener('click', (e) => {
+        let res = localStorage.getItem('respuesta')
+
+        if (res == 'incorrecto') {
+            localStorage.removeItem('respuesta');
+            vida--
+            vidas(vida);
+            incorrectoSelImg();
+        }
+        if (res === 'correcto') {
+            localStorage.removeItem('respuesta');
+            correcto();
+        }
+        e.stopPropagation();
+    })
+
+}
+
+const correcto = () => {
+    console.log('correcto')
+    footer.setAttribute('class', 'messageGood');
+    footer.innerHTML = '';
+    fragment.appendChild(templateGood);
+    footer.appendChild(fragment);
+    let continuar = document.getElementById('continue');
+    continuar.addEventListener('click', () => {
+        location.reload();
+    })
+
+}
+
+const incorrectoSel = () => {
+    let r = localStorage.getItem('good')
+    let pinta = document.querySelector('.temp');
+    pinta.setAttribute('class', 'a incorrect');
+    console.log(`la respuesta es ${r}`)
+    footer.setAttribute('class', 'messageBad');
+    footer.innerHTML = '';
+    templateBad.querySelector('span').textContent = r;
+    fragment.appendChild(templateBad);
+    footer.appendChild(fragment);
+    let continuar = document.getElementById('inContinue');
+    continuar.addEventListener('click', () => {
+        location.reload();
+    })
+}
+
+const incorrectoSelImg = () => {
+    let r = localStorage.getItem('good')
+    let pinta = document.querySelector('.temp');
+    pinta.setAttribute('class', 'b incorrecto');
+    console.log(`la respuesta es ${r}`)
+    footer.setAttribute('class', 'messageBad');
+    footer.innerHTML = '';
+    templateBad.querySelector('span').textContent = r;
+    fragment.appendChild(templateBad);
+    footer.appendChild(fragment);
+    let continuar = document.getElementById('inContinue');
+    continuar.addEventListener('click', () => {
+        location.reload();
+    })
+}
 
 const pintarBoton = () => {
-    menu.querySelector('button').setAttribute('id', 'comprobarCambio');
+    footer.querySelector('button').setAttribute('id', 'comprobarCambio');
 }
 
 const progresion = (porcentaje) => {
@@ -171,7 +389,6 @@ const progresion = (porcentaje) => {
 }
 
 const vidas = (vida) => {
-    console.log(vida)
     if (vida <= 0) {
         console.log('Game Over')
         localStorage.clear();
